9daeba6029549bbc873d57174cecb259d646480d
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2024-02-29 21:59:06 +0200

    Initialise users service project

---
+++ backend/usersservice/src/main/java/com/zodiaczen/utils/DateUtils.java
@@ -0,0 +1,31 @@
+package com.zodiaczen.utils;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.OffsetTime.now;
+
+/**
+ * Class used for defining the util methods/constants for dates.
+ */
+public class DateUtils {
+
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
+
+    /**
+     * Obtains the current date-time from the system clock in the default time-zone and return a OffsetDateTime having
+     * the DATE_TIME_PATTERN.
+     */
+    @NotNull
+    public static OffsetDateTime getDateTime() {
+        String dateTimeString = now().toString();
+        String dateTimeWithTimeZoneString = dateTimeString + "Z";
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(dateTimeWithTimeZoneString);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+        return OffsetDateTime.parse(zonedDateTime.format(formatter));
+    }
+}
