2e55b6800042d31dff44535249ca78bcbf010856
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2024-03-01 15:40:00 +0200

    Refactor

--- backend/usersservice/src/main/java/com/zodiaczen/exceptions/EmailSendingException.java
+++ backend/usersservice/src/main/java/com/zodiaczen/exceptions/EmailSendingException.java
@@ -1,13 +1,11 @@
 package com.zodiaczen.exceptions;
 
-import jakarta.mail.MessagingException;
-
 /**
  * Exception thrown when there is a problem sending an email.
  */
 public class EmailSendingException extends RuntimeException {
 
-    public EmailSendingException(String message, MessagingException e) {
+    public EmailSendingException(String message) {
         super(message);
     }
 }
