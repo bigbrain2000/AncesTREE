0adc14f582af217143bd27336b4723f258de10f5
ChiforAlexandru <alexandru.chifor@student.upt.ro>
2024-03-21 21:18:12 +0200

    Home folder added

---
+++ mobile/WeatherBeacon/app/src/main/java/com/upt/weatherBeacon/ui/home/HomeViewModel.java
@@ -0,0 +1,8 @@
+package com.upt.weatherBeacon.ui.home;
+
+import com.upt.weatherBeacon.ui.base.BaseFragment;
+import com.upt.weatherBeacon.ui.base.BaseViewModel;
+
+public class HomeViewModel extends BaseViewModel {
+
+}
