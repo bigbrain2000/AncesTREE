2d04bba9eb38bd7ba3a63635236e85a966bd5913
Roncea Marius-Alexandru <ronceamarius84@yahoo.com>
2024-05-02 15:57:45 +0300

    Add load tests.

--- backend/weatherservice/src/gatling/java/com/weatherbeaconboard/v1/WeatherLoadTest.java
+++ backend/weatherservice/src/gatling/java/com/weatherbeaconboard/v1/WeatherLoadTest.java
@@ -41,37 +41,37 @@ public class WeatherLoadTest extends Simulation {
                         .queryParam(LATITUDE, randomLatitudeValue)
                         .queryParam(LONGITUDE, randomLongitudeValue)
                         .check(status().is(EXPECTED_OK_STATUS_CODE)))
-                .pause(1)  // pause for a second between requests
+                .pause(5)  // pause for a second between requests
                 .exec(http("Flood Statistics Request")
                         .get(GET_FLOOD_STATISTICS_URL)
                         .queryParam(LATITUDE, randomLatitudeValue)
                         .queryParam(LONGITUDE, randomLongitudeValue)
                         .queryParam("daily", "river_discharge")
-//                        .queryParam("daily", "river_discharge_mean")
-//                        .queryParam("daily", "river_discharge_median")
-//                        .queryParam("daily", "river_discharge_max")
-//                        .queryParam("daily", "river_discharge_min")
-//                        .queryParam("daily", "river_discharge_p25")
-//                        .queryParam("daily", "river_discharge_p75")
+                        .queryParam("daily", "river_discharge_mean")
+                        .queryParam("daily", "river_discharge_median")
+                        .queryParam("daily", "river_discharge_max")
+                        .queryParam("daily", "river_discharge_min")
+                        .queryParam("daily", "river_discharge_p25")
+                        .queryParam("daily", "river_discharge_p75")
                         .queryParam("timeformat", "iso8601")
                         .queryParam("past_days", "30")
                         .queryParam("forecast_days", "210")
                         .queryParam("ensemble", "true")
                         .queryParam("cell_selection", "nearest")
                         .check(status().is(EXPECTED_OK_STATUS_CODE)))
-                .pause(1)   // pause for a second between requests
+                .pause(5)   // pause for a second between requests
                 .exec(http("Elevation Statistics Request")
                         .get(GET_ELEVATION_STATISTICS_URL)
                         .queryParam(LATITUDE, randomLatitudeValue)
                         .queryParam(LONGITUDE, randomLongitudeValue)
                         .check(status().is(EXPECTED_OK_STATUS_CODE)))
-                .pause(1)   // pause for a second between requests
+                .pause(5)   // pause for a second between requests
                 .exec(http("Air Quality Statistics Request")
                         .get(GET_AIR_QUALITY_STATISTICS_URL)
                         .queryParam(LATITUDE, randomLatitudeValue)
                         .queryParam(LONGITUDE, randomLongitudeValue)
                         .check(status().is(EXPECTED_OK_STATUS_CODE)))
-                .pause(1)   // pause for a second between requests
+                .pause(5)   // pause for a second between requests
                 .exec(http("Climate Statistics Request")
                         .get(GET_CLIMATE_STATISTICS_URL)
                         .queryParam(LATITUDE, randomLatitudeValue)
@@ -80,7 +80,8 @@ public class WeatherLoadTest extends Simulation {
                         .queryParam("end_date", "2024-12-31")
                         .queryParam("models", "CMCC_CM2_VHR4,FGOALS_f3_H,HiRAM_SIT_HR,MRI_AGCM3_2_S,EC_Earth3P_HR,MPI_ESM1_2_XR,NICAM16_8S")
                         .queryParam("daily", "temperature_2m_max")
-                        .check(status().is(EXPECTED_OK_STATUS_CODE)));
+                        .check(status().is(EXPECTED_OK_STATUS_CODE)))
+                .pause(5);
 
         setUp(scn
                 .injectClosed(rampConcurrentUsers(0).to(10).during(Duration.of(1, ChronoUnit.MINUTES)))
@@ -88,8 +89,11 @@ public class WeatherLoadTest extends Simulation {
         )
                 .maxDuration(Duration.of(3, ChronoUnit.MINUTES))
                 .assertions(
-                        details("99% of requests are successful within 3 seconds").responseTime().percentile(99).lte(3_000),
-                        details("95% of requests are successful within 5 seconds").responseTime().percentile(95).lte(5_000)
+                        details("Forecast Request").responseTime().percentile(99).lte(3_000),
+                        details("Flood Statistics Request").responseTime().percentile(99).lte(3_000),
+                        details("Elevation Statistics Request").responseTime().percentile(99).lte(3_000),
+                        details("Air Quality Statistics Request").responseTime().percentile(99).lte(3_000),
+                        details("Climate Statistics Request").responseTime().percentile(99).lte(3_000)
                 );
     }
 }
